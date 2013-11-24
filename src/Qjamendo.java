79ad43e1196483ee72738a01abbbc0550c0b5a9f
yves001 <yves.mercadier@gmail.com>
2013-11-24 09:27:37 +0100

    commit 24/11

--- src/Qjamendo.java
+++ src/Qjamendo.java
@@ -1,23 +1,10 @@
 
-import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
-import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
-import com.hp.hpl.jena.query.ResultSetFormatter;
-import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.vocabulary.RDF;
-import com.hp.hpl.jena.util.FileManager;
-import java.util.ArrayList;
-import java.util.List;
-import com.hp.hpl.jena.rdf.model.*;
-import com.hp.hpl.jena.query.*;
-import java.io.InputStream;
-import com.hp.hpl.jena.query.QueryExecution;
-import com.hp.hpl.jena.query.QueryExecutionFactory;
-import com.hp.hpl.jena.sparql.engine.http.QueryExceptionHTTP;
+
 
 public class Qjamendo{
 
@@ -29,22 +16,31 @@ public class Qjamendo{
 	      String sQueries="" ;
 	      String sSelect="";
 	      String sWhere="";
+	      String artiste="vincent j";
+	      
 		  sQueries =sQueries + "PREFIX rdf: <" + RDF.getURI() + ">" + CRLF;
 			  
 		  sQueries =sQueries + "PREFIX mo:<http://purl.org/ontology/mo/>" + CRLF;
 		  sQueries =sQueries + "PREFIX dce: <http://purl.org/dc/elements/1.1/>" + CRLF;
 		  sQueries =sQueries + "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>" + CRLF;
 		  sQueries =sQueries + "PREFIX foaf:  <http://xmlns.com/foaf/0.1/>" + CRLF;
-		 
+		  sQueries =sQueries + "PREFIX owl: <http://www.w3.org/2002/07/owl#>" + CRLF;
+		  sQueries =sQueries + "PREFIX tags: <http://www.holygoat.co.uk/owl/redwood/0.1/tags/>" + CRLF;
+
 		  sSelect="*";
 		  sQueries=sQueries+ "SELECT " + sSelect + CRLF;
 		  
 		  
-		 sWhere="?artist a mo:MusicArtist ;";
-		 sWhere=sWhere + " foaf:homepage ?homepage ;";
-		 sWhere=sWhere + "foaf:img ?img ;";
-		 sWhere=sWhere + "foaf:made ?made ;";
-		 sWhere=sWhere + " foaf:name \"vincent j\" .";
+		  sWhere="?artist a mo:MusicArtist ;";
+		  sWhere=sWhere + " foaf:homepage ?homepage ;";
+		  sWhere=sWhere + " foaf:based_near ?based ;";
+		  //sWhere=sWhere + " owl:sameAs ?sameas ;";
+		  //sWhere=sWhere + " mo:biography ?bio ;";
+		  
+		  sWhere=sWhere + " foaf:img ?img ;";
+		  sWhere=sWhere + " foaf:made ?made ;";
+		  //sWhere=sWhere + " tags:taggedWithTag ?tag ;";		
+		  sWhere=sWhere + " foaf:name \""+artiste+"\" .";
 		
 		 
 		  sQueries = sQueries+ "WHERE { "+sWhere+" } ";
@@ -62,6 +58,9 @@ public class Qjamendo{
 					System.out.println("homepage : "+soln.get("?homepage"));
 					System.out.println("image : "+soln.get("?img"));
 					System.out.println("album  : "+soln.get("?made"));
+					System.out.println("lieu de résidence  : "+soln.get("?based"));
+					//System.out.println("biographie  : "+soln.get("?bio"));
+					System.out.println("voir aussi  : "+soln.get("?sameas"));
 					System.out.println();
 				}
 		 }
@@ -79,20 +78,25 @@ public class Qjamendo{
 		  
 		  
 		  
-		  
+		  String record="http://dbtune.org/jamendo/record/1186";
 		  sQueries ="PREFIX rdf: <" + RDF.getURI() + ">" + CRLF;		  
 		  sQueries =sQueries + "PREFIX mo:<http://purl.org/ontology/mo/>" + CRLF;
 		  sQueries =sQueries + "PREFIX dce: <http://purl.org/dc/elements/1.1/>" + CRLF;
+		  sQueries =sQueries + "PREFIX dc: <http://purl.org/dc/elements/1.1/>" + CRLF;
+		  
+		  sQueries =sQueries + "PREFIX dce: <http://purl.org/dc/elements/1.1/>" + CRLF;
 		  sQueries =sQueries + "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>" + CRLF;
 		  sQueries =sQueries + "PREFIX foaf:  <http://xmlns.com/foaf/0.1/>" + CRLF;
-		 
+		  sQueries =sQueries + "PREFIX tags: <http://www.holygoat.co.uk/owl/redwood/0.1/tags/>" + CRLF;
 		  sSelect="*";
 		  sQueries=sQueries+ "SELECT " + sSelect + CRLF;
 		  
-		 sWhere="<http://dbtune.org/jamendo/record/1186> a mo:Record ;";
-		 sWhere=sWhere + " dce:date ?date ;";
-		 sWhere=sWhere + "mo:available_as ?lien;";
-		 sWhere=sWhere + "dce:title ?title .";		
+		  sWhere="<"+record+"> a mo:Record ;";
+		  sWhere=sWhere + "dce:date ?datedce ;";
+		  sWhere=sWhere + "dc:date ?datedc ;";
+		  sWhere=sWhere + "mo:available_as ?lien;";
+		  sWhere=sWhere + "dce:title ?title ;";		
+		  sWhere=sWhere + "tags:taggedWithTag ?tag .";		
 		 
 		  sQueries = sQueries+ "WHERE { "+sWhere+" } ";
 			 
@@ -105,8 +109,12 @@ public class Qjamendo{
 				{
 					QuerySolution soln = rs.nextSolution();
 					System.out.println("titre : "+soln.get("?title"));
-					System.out.println("date : "+soln.get("?date"));
+					System.out.println("datedce : "+soln.get("?datedce"));
+					System.out.println("datedc : "+soln.get("?datedc"));
 					System.out.println("lien : "+soln.get("?lien"));
+					System.out.println("tag : "+soln.get("?tag"));
+					System.out.println();
+					System.out.println();
 					System.out.println();
 				}
 		 }
